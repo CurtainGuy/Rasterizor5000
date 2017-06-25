bfb3a00e0d1963b134d77b2c1b265edd75e1018f
Thomas Wojcik <thomas-wojcik@hotmail.com>
2017-06-25 16:07:38 +0200

    vignetting en color aberation FIX

--- shaders/fs_post.glsl
+++ shaders/fs_post.glsl
@@ -11,20 +11,22 @@ out vec3 outputColor;
 void main()
 {
 	// retrieve input pixel
-	outputColor = texture( pixels, uv ).rgb;
+	outputColor.x = texture(pixels, vec2(uv.x, uv.y)).x;
+	outputColor.y = texture(pixels, vec2(uv.x + 0.002f, uv.y)).y;
+	outputColor.z = texture(pixels, vec2(uv.x, uv.y + 0.002f)).z;
+
 	// retrieve brightness
 	vec3 HDRBloom = max(outputColor - vec3(0.90f, 0.90f, 0.90f), vec3(0.0f, 0.0f, 0.0f));
+
 	// retrieve distance
 	float dx = P.x - 0.5, dy = P.y - 0.5;
 	float distance = sqrt( dx * dx + dy * dy );
 
-	// apply vignetting and chromatic abberation
-	outputColor.r *= 1 / (distance + 0.20f) * 0.25f;
-	outputColor.g *= 1 / (distance + 0.15f) * 0.25f;
-	outputColor.b *= 1 / (distance + 0.10f) * 0.25f;
+	// apply vignetting
+	outputColor *= 1 / (distance + 0.25f) * 0.25f;
 	
 	// apply Gaussian blur
-	float s = 0.44089642f, e = 2.71828f, pi = 3.14159f; // these values have been taken from wikipedia
+	float s = 0.84089642f, e = 2.71828f, pi = 3.14159f;			 // these values have been taken from wikipedia
 	HDRBloom *= (1/ (2 * pi * s * s) * pow(e, (-dx * dx - dy * dy) / (2 * s * s)));
 	// apply HDR Bloom
 	outputColor += HDRBloom;
